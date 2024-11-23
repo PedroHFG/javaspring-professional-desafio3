8cfb95403097700a08d0f5466d57e7d5f56afab5
PedroHFG <pedrohfidg@gmail.com>
2024-11-23 15:21:20 -0300

    Created ClientService

---
+++ src/main/java/com/devsuperior/desafio3/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.desafio3.services;
+
+import com.devsuperior.desafio3.dto.ClientDTO;
+import com.devsuperior.desafio3.entities.Client;
+import com.devsuperior.desafio3.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+}
